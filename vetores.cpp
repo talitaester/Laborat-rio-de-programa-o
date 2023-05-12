ec4e1039c6350ed3528b483dae203f7a369fd2f1
Talita Ester <talita.alves@ufba.br>
2023-05-12 19:46:52 +0000

    new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   random.cpp 	new file:   vetores.cpp

---
+++ vetores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    vector<int> ages;
+    for (0; i < 10; i++) {
+    cout << "Digite a idade do pesquisador da vez" << endl;
+    cin << age;
+    ages.push_back();
+    }
+}
