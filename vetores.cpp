529f501b239e2008c66f9770cc5811b7bb0cdcfb
Talita Ester <talita.alves@ufba.br>
2023-05-12 20:29:23 +0000

    modified:   .vscode/settings.json 	deleted:    empty 	new file:   lista 1/q1 	deleted:    random.cpp 	modified:   vetores.cpp

--- vetores.cpp
+++ vetores.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 
 int main(){
-    vector<int> ages;
-    for (0; i < 10; i++) {
+  int i=0, age;
+  vector<int> ages;
+  for (0; i < 10; i++) {
     cout << "Digite a idade do pesquisador da vez" << endl;
-    cin << age;
-    ages.push_back();
+    cin >> age;
+    ages.push_back(age);    
+  }
+  int t=0;
+  for (int t = 0; t < ages.size(); t++) {
+    cout << ages[t] << " ";
     }
+    cout << endl;
 }
